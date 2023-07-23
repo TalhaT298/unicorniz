9f38ba49a1dd41999c7df8464dfa26d03ad6c46e
im-paul-mak33 <souravpaul210789@gmail.com>
2023-07-23 14:45:57 +0530

    design and content structure made responsive

--- pages/components/Home/Testimonial/Testimonial.js
+++ pages/components/Home/Testimonial/Testimonial.js
@@ -34,12 +34,11 @@ const Testimonial = () => {
 	];
 	return (
 		<div className="md:mt-[90px] mt-[40px]">
-			<div className="flex justify-between items-center md:mx-[10px]">
-				<h3 className="uppercase text-[40px] pl-4 lg:pl-0 md:pl-0">
+			<div className="flex justify-between items-center">
+				<h3 className="uppercase text-[32px] sm:text-[40px]">
 					Testimonials <hr className="w-16 border border-black" />
-
 				</h3>
-				<p className="text-[#FF7E21] font-bold cursor-pointer">Know More</p>
+				<p className="text-[#FF7E21] font-bold cursor-pointer text-center">Know More</p>
 			</div>
 
 			<div className="carousel overflow-hidden">
@@ -60,19 +59,19 @@ const Testimonial = () => {
 					return (
 						<div
 							key={item.id}
-							className={` ${className} md:min-w-[740px] min-h-[348px] min-w-[460px] md:p-[20px] p-4 bg-[#FAFAFA]`}>
+							className={`${className} md:min-w-[740px] min-h-[300px] min-w-[460px] md:p-[20px] p-4 bg-[#FAFAFA]`}>
 							<div className="flex md:gap-[31px] gap-3 justify-center items-center w-full">
-								<img src={item.image} className="w-[160px] h-[200px] md:h-auto md:w-full" />{" "}
-								<div className="w-auto">
+								<img src={item.image} className="w-1/4 sm:w-2/7 mt-10" />
+								<div className="w-full">
 									<div className="flex justify-end">
 										<Image className="invisible" src={quotes} alt="quotes" />
 									</div>
-									<p className="items-center justify-center text-sm">
+									<div className="items-center justify-center text-sm w-3/4 sm:w-auto">
 										Aut nihil mollitia deserunt quia sed rem. Quibusdam amet
 										veniam rerum id rerum beatae. Quas rerum iste
 										necessitatibus. At voluptates ad magnam blanditiis excepturi
 										expedita aut. Aut repellat inventore qui minima illum est.
-									</p>
+									</div>
 									<h6 className="font-semibold mt-5">Kyle Merwin</h6>
 									<small className="text-[14px] text-[#808080]">CO-owner</small>
 								</div>
@@ -83,7 +82,7 @@ const Testimonial = () => {
 			</div>
 
 			<div
-				className={`flex gap-5 w-[278px] mx-auto md:mb-[101px] mb-10 lg:mt-[5vw] mt-[10vw]`}>
+				className={`flex gap-5 w-[278px] mx-auto mb-10 mt-0 md:mt-5`}>
 				<div className="flex items-center gap-1">
 					<span className="font-extrabold cursor-pointer">0{index + 1}</span>{" "}
 					<hr className="w-20 border border-black" />
